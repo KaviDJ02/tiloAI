4d47f2d11c8e3f172e04a0251eb426874661b899
Dinidu Sachintha <dinidusachintha3@gmail.com>
2024-12-05 21:41:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Page1 from "./pages/Page1"
+import Page2 from "./pages/Page2"
+import Page3 from "./pages/Page3"
+
+
+function App() {
+ 
+
+  return (
+   <>
+     <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="ws2" element={<Page1 />} />
+          <Route path="ws3" element={<Page2 />} />
+          <Route path="ws4" element={<Page3 />} />
+        </Routes>
+     </BrowserRouter>
+   </>
+  )
+}
+
+export default App
